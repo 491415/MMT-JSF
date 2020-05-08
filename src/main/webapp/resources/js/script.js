8f4b340c7e5261c625642bfa5d6e5abfcb230d24
dkralj <kraljdino.x@gmail.com>
2020-05-08 12:01:44 +0200

    added script for image resizing

---
+++ src/main/webapp/resources/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("#image1").on("click", function() {
+		$("#image1").toggleClass("img-full");
+		;
+	});
+	$("#image2").on("click", function() {
+		$("#image2").toggleClass("img-full");
+		;
+	});
+});
